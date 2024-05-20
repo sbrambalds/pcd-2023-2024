433b8cecd0299b9fe27f86f94bc38c895a941f29
Alessandro Ricci <a.ricci@unibo.it>
2024-05-20 12:21:43 +0200

    Lab Activity #12

---
+++ app/src/main/java/pcd/lab10/mom/rabbitmq/Test0_Recv.java
@@ -0,0 +1,32 @@
+package pcd.lab10.mom.rabbitmq;
+
+import com.rabbitmq.client.*;
+
+public class Test0_Recv {
+
+	private final static String QUEUE_NAME = "hello";
+
+	public static void main(String[] argv) throws Exception {
+
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("localhost");
+		Connection connection = factory.newConnection();
+
+		Channel channel = connection.createChannel();
+
+		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+		System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+		DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+			String message = new String(delivery.getBody(), "UTF-8");
+			System.out.println(" [x] Received '" + message + "' by thread: " + Thread.currentThread().getName());
+		};
+
+		boolean autoAck = true;
+		String consumerTag = channel.basicConsume(QUEUE_NAME, autoAck, deliverCallback,
+				/* cancellation callback */ consTag -> {
+				});
+
+		System.out.println("Consumer configured - tag: " + consumerTag);
+	}
+}
